6d1119b156b2475f160a314073890e73e8da2f05
NaveenKarippai <naveenranjankarippai@gmail.com>
2018-04-08 00:54:35 +0200

    Switch to sane theme and fix compile error

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,43 +19,21 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Favorite Song', 'https://www.youtube.com/watch?v=rMnunePFRVU'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = "./theme"
+THEME = "../MinimalXY"
 
 # organize your articles in subfolders
-USE_FOLDER_AS_CATEGORY = False
 DEFAULT_CATEGORY = 'General'
 
 TYPOGRIFY = True
 SUMMARY_MAX_LENGTH = 50
 NEWEST_FIRST_ARCHIVES = True
 
-
 LOAD_CONTENT_CACHE = False
-PLUGIN_PATHS = ['../pelican-plugins/']
-PLUGINS = ['i18n_subsites']
-
-# mapping: language_code -> settings_overrides_dict
-I18N_SUBSITES = {
-    'cz': {
-        'SITENAME': 'Hezkej blog',
-	 'THEME' : "./theme/waterspill"
-        }
-    }
 
 STATIC_PATHS = [
     'extra/robots.txt',
@@ -69,4 +47,22 @@ EXTRA_PATH_METADATA = {
 
 DISPLAY_PAGES_ON_MENU = False
 DISPLAY_CATEGORIES_ON_MENU = True
+
+# Social widget
+SOCIAL = (('twitter', 'https://twitter.com/yourprofile'),
+         ('instagram', 'https://instagram.com/yourprofile'),
+         ('github', 'https://github.com/yourprofile'))
+
+SHARE_BUTTONS = ('twitter', 'facebook', 'whatsapp', 'mail')
+ 
+# TAG_CLOUD plugin
+TAG_CLOUD = True
+TAG_CLOUD_STEPS = 5
+TAG_CLOUD_MAX_ITEMS = 50
+TAG_CLOUD_BADGE = True
+TAG_CLOUD_SORTING = 'size'
+
+# Plugins
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ["tag_cloud"]
 
--- two.md
+++ two.md
@@ -1,5 +1,4 @@
-Title: Article1 
-tags: fun, pelican, plugins
+Title: Article3 tags: pelican, plugins
 Date: 2010-12-03 10:20
 Following is a review of my favorite mechanical keyboard.
 
