8e5287d64c00c314e20e426029b66ee55c092fff
NaveenKarippai <naveenranjankarippai@gmail.com>
2018-04-08 16:52:53 +0200

    Fix http security issue

--- pelicanconf.py
+++ pelicanconf.py
@@ -77,7 +77,7 @@ MENUITEMS = (
 # Author
 AUTHOR_INTRO = 'Hello world! I’m Maru.'
 AUTHOR_DESCRIPTION = 'Hello world! I’m Maru. I like coffee, birds and Python.'
-AUTHOR_AVATAR = 'http://www.gravatar.com/avatar/abcdefghijkl?s=240'
+AUTHOR_AVATAR = 'https://www.gravatar.com/avatar/abcdefghijkl?s=240'
 
 # Theme customizations
 MINIMALXY_START_YEAR = 2018
