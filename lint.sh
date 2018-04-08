c0992d432f973354cab3fefef1a605f849dc24a4
NaveenKarippai <naveenranjankarippai@gmail.com>
2018-04-08 19:47:57 +0200

    Fix shell script for linters

--- lint.sh
+++ lint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ev
-[ -f content/*/*.md ] && hint content/*/*.md
-[ -f content/*/*.rst ] && rst-lint content/*/*.rst
-[ -f content/*.md ] && hint content/*.md
-[ -f content/*.rst ] && rst-lint content/*.rst
+md2=`ls -l content/*/*.md 2>/dev/null | wc -l`
+md1=`ls -l content/*.md 2>/dev/null | wc -l`
+rst2=`ls -l content/*/*.rst 2>/dev/null | wc -l`
+rst1=`ls -l content/*.rst 2>/dev/null | wc -l`
+
+[ $md2 != 0 ] && hint content/*/*.md
+[ $md1 != 0 ] && hint content/*.md
+[ $rst1 != 0 ] && rst-lint content/*.rst
+[ $rst2 != 0 ] && rst-lint content/*/*.rst
