0e70fb3da8590fc0e3eff5cb4cbefed9733f334c
NaveenKarippai <naveenranjankarippai@gmail.com>
2018-04-11 22:17:36 +0200

    Fix lint path

--- lint.sh
+++ lint.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 set -ev
-md2=`ls -l content/*/*.md 2>/dev/null | wc -l`
-md1=`ls -l content/*.md 2>/dev/null | wc -l`
-rst2=`ls -l content/*/*.rst 2>/dev/null | wc -l`
-rst1=`ls -l content/*.rst 2>/dev/null | wc -l`
 
-if [ $md2 -ne 0 ]; then
-	 hint content/*/*.md
-fi
-if [ $rst1 -ne 0 ]; then
- 	rst-lint content/*.rst
-fi
-if [ $rst2 -ne 0 ]; then
- 	rst-lint content/*/*.rst
-fi
-if [ $md1 -ne 0 ]; then
-	hint content/*.md
-fi
+hint content/
+rst-lint content/
