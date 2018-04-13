649e822977d45c80b6c949cece42820e3b5c8192
Naveen Karippai <naveenranjankarippai@gmail.com>
2018-04-13 18:06:43 +0200

    Disqus: Set sane sitename

--- pelicanconf.py
+++ pelicanconf.py
@@ -83,7 +83,7 @@ SITEMAP = {
 }
 
 # Disqus plugin
-DISQUS_SITENAME = 'naveenkarippai.github.io'
+DISQUS_SITENAME = 'naveenkarippai'
 DISQUS_SECRET_KEY = os.environ["DISQUS_SECRET_KEY"]
 DISQUS_PUBLIC_KEY = os.environ["DISQUS_PUBLIC_KEY"]
 
